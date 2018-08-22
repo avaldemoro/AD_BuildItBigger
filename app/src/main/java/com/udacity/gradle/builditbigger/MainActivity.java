bbe9c5b72930dcbbf36b2b1a50fe3833ff42fd8c
Aster Valdemoro <astervaldemoro@gmail.com>
2018-08-22 10:38:02 -0400

    fix joke android library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,17 +8,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import co.asterv.jokeandroidlibrary.DisplayJokeActivity;
 import co.asterv.jokelibrary.Joke;
 
 
 public class MainActivity extends AppCompatActivity {
     private Joke joke;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         joke = new Joke();
+        intent = new Intent(this, DisplayJokeActivity.class);
+
     }
 
 
@@ -44,7 +49,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, joke.getRandomJoke(), Toast.LENGTH_LONG).show();
+        String jokeString = joke.getRandomJoke ();
+        //Toast.makeText(this, joke.getRandomJoke(), Toast.LENGTH_LONG).show();
+        intent.putExtra (DisplayJokeActivity.JOKE_INTENT, jokeString);
+        intent.addFlags (Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 
--- jokeandroidlibrary/src/main/java/co/asterv/jokeandroidlibrary/DisplayJokeActivity.java
+++ jokeandroidlibrary/src/main/java/co/asterv/jokeandroidlibrary/DisplayJokeActivity.java
@@ -1,6 +1,19 @@
 package co.asterv.jokeandroidlibrary;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DisplayJokeActivity extends AppCompatActivity {
+    public final static String JOKE_INTENT = "JOKE_INTENT";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView(R.layout.activity_display_joke);
+
+        String joke = getIntent().getStringExtra (JOKE_INTENT);
+        TextView jokeTV = findViewById (R.id.jokeTextView);
+        jokeTV.setText(joke);
+    }
 }
